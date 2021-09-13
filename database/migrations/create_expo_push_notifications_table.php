ce9e6386eeabbb8b485ddf3df73572b18366d345
trin <trin4ik@gmail.com>
2021-09-13 05:01:32 +0300

    migration fix

--- database/migrations/create_expo_push_notifications_table.php
+++ database/migrations/create_expo_push_notifications_table.php
@@ -6,28 +6,28 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateExpoPushNotificationsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up()
-    {
-        Schema::create(app(config('expo-push.log.driver.instance'))->getTable(), function (Blueprint $table) {
-            $table->id();
-            $table->string('item_type')->nullable();
-            $table->string('item_id')->nullable();
-            $table->text('payload')->nullable();
-            $table->text('response')->nullable();
-            $table->unsignedSmallInteger('code')->default(0)->index();
-            $table->char('status', 50)->nullable()->index();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 */
+	public function up()
+	{
+		Schema::create(app(config('expo-push.log.driver.instance'))->getTable(), function (Blueprint $table) {
+			$table->id();
+			$table->string('item_type')->nullable();
+			$table->string('item_id')->nullable();
+			$table->text('payload')->nullable();
+			$table->text('response')->nullable();
+			$table->unsignedSmallInteger('code')->default(0)->index();
+			$table->string('status')->nullable()->index();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down()
-    {
-        Schema::drop(app(config('expo-push.log.driver.instance'))->getTable());
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down()
+	{
+		Schema::drop(app(config('expo-push.log.driver.instance'))->getTable());
+	}
 }
